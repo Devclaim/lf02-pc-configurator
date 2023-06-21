a9b013ed18ba4f49855d4904e14c7b58cd1648a6
Deathclaim <al.abdullah@outlook.de>
2023-06-21 22:53:45 +0200

    refactor

---
+++ src/Compatibility/CompatibilityFunctions.ts
@@ -0,0 +1,22 @@
+import { CPU, ComputerComponent, Cooler, GPU, Motherboard, PowerSupply, RAM } from "../RawData/RawDataInterfaces";
+
+export function cpuIsCompatible(cpu: CPU, motherboard: Motherboard): boolean {
+    return cpu.socket === motherboard.socket && cpu.powerRequirement <= motherboard.powerRequirement;
+}
+
+export function gpuIsCompatible(gpu: GPU, motherboard: Motherboard): boolean {
+    return gpu.powerRequirement <= motherboard.powerRequirement;
+};
+
+export function ramIsCompatible(ram: RAM, motherboard: Motherboard): boolean {
+    return ram.ramType === motherboard.ramType;
+};
+
+export function coolerIsCompatible(cooler: Cooler, cpu: CPU): boolean {
+    return cooler.socket === cpu.socket;
+}
+
+export function powerSupplyIsCompatible(powerSupply: PowerSupply, components: ComputerComponent[]): boolean {
+    const totalPowerRequirement = components.reduce((total, component) => total + (component.powerRequirement || 0), 0);
+    return powerSupply.wattage >= totalPowerRequirement;
+}
